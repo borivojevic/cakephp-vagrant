17ff415805fa178d186247b00e83490812999fa9
Mirko Borivojevic <mirko.borivojevic@gmail.com>
2012-08-16 12:48:53 +0200

    Add apache2 setup step

--- vagrant_php/puppet/base.pp
+++ vagrant_php/puppet/base.pp
@@ -19,9 +19,46 @@ class ubuntu {
     }
 }
 
+class apache2 {
+    package { "apache2": ensure => installed }
+
+    $config = '/etc/apache2/sites-available/default'
+    exec { "servername":
+        subscribe => Package["apache2"],
+        unless  => "grep '^ServerName' /etc/apache2/apache2.conf",
+        command => "echo ServerName app >> /etc/apache2/apache2.conf",
+        notify  => Service["apache2"],
+    } ->
+    exec { "mod-rewrite":
+        unless => "ls /etc/apache2/mods-enabled/rewrite*",
+        command => "a2enmod rewrite",
+        notify  => Service["apache2"],
+    } ->
+    exec { "allow-override":
+        unless => "sed -n '11 p' $config | grep 'AllowOverride All'",
+        command => "sed -i '11 s/None/All/' $config",
+        notify  => Service["apache2"],
+    } ->
+    user { "www-data":
+        groups => ["vagrant"],
+        notify  => Service["apache2"],
+    } ->
+    file { "/var/www/app":
+        ensure => link,
+        target => "/vagrant/src/app",
+    } ->
+    service { "apache2":
+        ensure => running,
+        hasstatus => true,
+        hasrestart => true,
+        require => Package["apache2"],
+    }
+}
+
 # Create some synchronization between operations
 class cakephpbox {
     class { "ubuntu": }
+	class { "apache2": require => Exec['apt-get-update'], }
 }
 
 # Tell Puppet to run the cakephpbox class at boot time:
